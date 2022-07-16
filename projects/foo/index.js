c59c4b890caa2d20c0ea1e2595a9922a2114c7be
Andrey Bekrenev <bekrenev1991@gmail.com>
2022-07-16 18:18:27 +0300

    homeWork 1.1, 1.2
    Выполнение домашнего задания 1.1 и 1.2

---
+++ projects/foo/index.js
@@ -0,0 +1,15 @@
+// hw1
+// const fun = param => param;
+// console.log((5));
+
+// HW2
+// const fun2 = (a, b) => {
+//     return a + b;
+// }
+// const resul = fun2(2, 5);
+// console.log(resul);
+
+
+
+
+
